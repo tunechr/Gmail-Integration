77497906b3475a9e19b2e3bb7ef0f6dfc1041e9d
tune_chris@hotmali.com <tune_chris@hotmail.com>
2016-06-10 16:10:27 +1000

    Creating Google Authentication Example. This is  a basic example using FlowAuth class from google

---
+++ Google.Authentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Google.Authentication.Startup))]
+namespace Google.Authentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
